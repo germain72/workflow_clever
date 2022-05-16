90d5d87d3d88b283337ffc789fdc53d007ffd435
germain72 <a-vos-clics@wanadoo.fr>
2022-05-16 10:57:40 +0200

    ihm vue.py

---
+++ Tutorials-master/ihm/vue.py
@@ -0,0 +1,10 @@
+ Le volume dans le lecteur E s'appelle Data2
+ Le num‚ro de s‚rie du volume est 4878-762C
+
+ R‚pertoire de E:\ALL_Formations\0_clever_institut\git\drive-download-20220317T080854Z-001\projet\workflow_clever\Tutorials-master\ihm
+
+16/05/2022  10:57    <DIR>          .
+16/05/2022  10:57    <DIR>          ..
+16/05/2022  10:57                 0 vue.py
+               1 fichier(s)                0 octets
+               2 R‚p(s)  139ÿ027ÿ763ÿ200 octets libres
